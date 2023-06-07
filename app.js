307272f9945482c24fe97c0b0b9bbb276aaae084
shahaji-patil <110805466+shahaji-patil@users.noreply.github.com>
2023-06-07 12:42:32 +0400

    video streaming app

---
+++ app.js
@@ -0,0 +1,54 @@
+const { log } = require('console');
+const express  = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+// Set the port for the server
+const port = 3007;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+// Define the route to stream the video
+app.get('/video', (req, res) => {
+  const videoPath = path.join(process.cwd(), 'TypeORM-Crash-Course-TypeScript-and-Node-RM.mp4');
+  const stat = fs.statSync(videoPath);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+  console.log(stat);
+  console.log(range);
+
+  if (range) {
+    const parts = range.replace(/bytes=/, '').split('-');
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunkSize = end - start + 1;
+    const file = fs.createReadStream(videoPath, { start, end });
+    
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunkSize,
+      'Content-Type': 'video/mp4',
+    };
+console.log(head);
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+
+    res.writeHead(200, head);
+    fs.createReadStream(videoPath).pipe(res);
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
